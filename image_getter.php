96a0dc980b070f7246afd12168933380b9ab3013
Rosario B. <rossa_4057@hotmail.com>
2015-03-11 23:15:30 +0100

    Some Notes

--- image_getter.php
+++ image_getter.php
@@ -8,16 +8,18 @@
 
 	function getImage() {
 		//RL
+		/*
 		$imageBin = file_get_contents(URL_CAM_SNAPSHOT);
 		$base64   = base64_encode($imageBin); 
 		$myfile = fopen("image/snapshot.jpg", "w");
 		fwrite($myfile, $imageBin);
 		fclose($myfile);
 		echo '{"path" : "' . URL_IMAGE . '/snapshot.jpg"}';
+		*/
 		
 		//TEST
-		/*$imageBin = file_get_contents('image/default.png');
+		$imageBin = file_get_contents('image/default.png');
 		$base64 = base64_encode($imageBin);
-		echo '{"path" : "http://localhost:84/github/cam-ctrl/image/default.png"}';*/
+		echo '{"path" : "http://localhost:84/github/cam-ctrl/image/default.png"}';
 	}
 ?>
